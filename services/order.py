6128a9e6e598807504a3a64ecc8215b675535f00
Andrii <yurkivandriy02@gmail.com>
2025-03-22 00:18:56 +0200

    Completed task py-tickets-and-orders

---
+++ services/order.py
@@ -0,0 +1,41 @@
+import datetime
+
+from django.contrib.auth import get_user_model
+from django.db import transaction
+
+from db.models import Ticket, Order, MovieSession
+
+
+@transaction.atomic
+def create_order(
+        tickets: list[dict],
+        username: str,
+        date: datetime = None
+) -> Order:
+    user = get_user_model().objects.get(username=username)
+    order = Order.objects.create(user=user)
+    if date:
+        order.created_at = date
+        order.save()
+
+    ticket_objects = []
+    for ticket in tickets:
+        movie_session = MovieSession.objects.get(id=ticket["movie_session"])
+        ticket_ = Ticket(
+            row=ticket["row"],
+            seat=ticket["seat"],
+            order=order,
+            movie_session=movie_session
+        )
+        ticket_objects.append(ticket_)
+
+    Ticket.objects.bulk_create(ticket_objects)
+
+    return order
+
+
+def get_orders(username: str = None) -> Order:
+    if username:
+        user = get_user_model().objects.get(username=username)
+        return Order.objects.filter(user=user)
+    return Order.objects.all()
